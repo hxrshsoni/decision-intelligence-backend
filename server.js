fbc3575dfe3d3d3baaa1443f4044877ecf8909e3
hxrshsoni <harshkumar552002@gmail.com>
2026-01-08 02:45:22 +0530

    Fix: Remove helmet dependency

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const cors = require('cors');
-const morgan = require('morgan');
+// const morgan = require('morgan');
 const dotenv = require('dotenv');
 const Database = require('./src/db');
 const logger = require('./src/utils/logger');
